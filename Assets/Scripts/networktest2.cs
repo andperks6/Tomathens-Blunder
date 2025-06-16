f60f5ec155fa3659dbac42e172a8e0891d837140
RiskyBiscuitGames <riskybiscuitgames@gmail.com>
2025-06-15 22:38:43 -0700

    Allow for spawning outside of locked room

--- Assets/Scripts/networktest2.cs
+++ Assets/Scripts/networktest2.cs
@@ -5,56 +5,59 @@ using Mirror;
 
 public class networktest2 : NetworkBehaviour
 {
-    public GameObject[] races;
-    
-    public int boxident;
-    public startbuttons sb1;
-    public int whichRace;
-    public NetworkConnection con2;
-    public GameObject toad;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("im real");
-        
-    }
+	public GameObject[] races;
 
-    // Update is called once per frame
-    void Update()
-    {
-      
+	public int boxident;
+	public startbuttons sb1;
+	public int whichRace;
+	public NetworkConnection con2;
+	public GameObject toad;
 
-    }
-    
-    public void addplayer2()
-    {
-        //NetworkIdentity netId = NetworkClient.connection.identity;
-        Debug.Log(GetComponent<NetworkIdentity>().connectionToClient);
-        Debug.Log(GetComponent<NetworkIdentity>().connectionToServer);
-        ReplacePlayer(GetComponent<NetworkIdentity>().connectionToClient);
+	//took base value from playerstart pos game object
+	public Vector3 playerSpawnPosition = new Vector3(127.93f, -57.8f, 0);
+	// Start is called before the first frame update
+	void Start()
+	{
+		Debug.Log("im real");
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
 
 
-    }
-    public void ReplacePlayer(NetworkConnection conn)
-    {
-        // Cache a reference to the current player object
-        GameObject oldPlayer = conn.identity.gameObject;
+	}
 
-        // Instantiate the new player object based on the selected race and broadcast to clients
-        // Ensure whichRace is a valid index for the races array
-        GameObject playerPrefabToSpawn = toad; // Default to toad
-        if (races != null && whichRace >= 0 && whichRace < races.Length && races[whichRace] != null)
-        {
-            playerPrefabToSpawn = races[whichRace];
-        }
-        else
-        {
-            Debug.LogError($"Invalid race index ({whichRace}) or races array not set. Spawning default toad prefab.");
-        }
+	public void addplayer2()
+	{
+		//NetworkIdentity netId = NetworkClient.connection.identity;
+		Debug.Log(GetComponent<NetworkIdentity>().connectionToClient);
+		Debug.Log(GetComponent<NetworkIdentity>().connectionToServer);
+		ReplacePlayer(GetComponent<NetworkIdentity>().connectionToClient);
 
-        NetworkServer.ReplacePlayerForConnection(conn, Instantiate(playerPrefabToSpawn, transform.position, Quaternion.Euler(0, 0, 0)));
 
-        // Remove the previous player object that's now been replaced
-        NetworkServer.Destroy(oldPlayer);
-    }
+	}
+	public void ReplacePlayer(NetworkConnection conn)
+	{
+		// Cache a reference to the current player object
+		GameObject oldPlayer = conn.identity.gameObject;
+
+		// Instantiate the new player object based on the selected race and broadcast to clients
+		// Ensure whichRace is a valid index for the races array
+		GameObject playerPrefabToSpawn = toad; // Default to toad
+		if (races != null && whichRace >= 0 && whichRace < races.Length && races[whichRace] != null)
+		{
+			playerPrefabToSpawn = races[whichRace];
+		}
+		else
+		{
+			Debug.LogError($"Invalid race index ({whichRace}) or races array not set. Spawning default toad prefab.");
+		}
+
+		NetworkServer.ReplacePlayerForConnection(conn, Instantiate(playerPrefabToSpawn, playerSpawnPosition, Quaternion.Euler(0, 0, 0)));
+
+		// Remove the previous player object that's now been replaced
+		NetworkServer.Destroy(oldPlayer);
+	}
 }
